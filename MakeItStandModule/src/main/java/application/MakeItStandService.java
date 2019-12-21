c67395f31497ba9dcbcf60a4f9e739dde1d10051
Ethgard <34208588+Ethgard@users.noreply.github.com>
2019-12-21 15:08:42 +0200

    remove config and create main service

---
+++ MakeItStandModule/src/main/java/application/MakeItStandService.java
@@ -0,0 +1,17 @@
+package application;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MakeItStandService implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        main(args);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
+}
