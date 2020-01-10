d3846101a28b97eb6e12d85f981268c8672f6731
Ethgard <34208588+Ethgard@users.noreply.github.com>
2020-01-10 20:05:31 +0200

    add another ctor and setter with Axis

--- src/main/java/application/Voxel.java
+++ src/main/java/application/Voxel.java
@@ -15,6 +15,16 @@ public class Voxel {
         this(x[0], x[1], x[2]);
     }
 
+    public Voxel(Voxel o) {
+        if (o == null) {
+            return;
+        }
+        this.x = o.getX();
+        this.y = o.getY();
+        this.z = o.getZ();
+        this.alpha = o.getAlpha();
+    }
+
     public Voxel(int x, int y, int z) {
         this.x = x;
         this.y = y;
@@ -56,6 +66,22 @@ public class Voxel {
                 return z;
             default:
                 return -1;
+        }
+    }
+
+    public void set(Axis axis, int value) {
+        switch (axis) {
+            case X:
+                setX(value);
+                break;
+            case Y:
+                setY(value);
+                break;
+            case Z:
+                setZ(value);
+                break;
+            default:
+                break;
         }
     }
 
