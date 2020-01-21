ce16e571a6d8378a801c3a4d4a760a20d4034e43
Ethgard <34208588+Ethgard@users.noreply.github.com>
2020-01-21 09:33:41 +0200

    use new classes in service

--- src/main/java/application/MakeItStandService.java
+++ src/main/java/application/MakeItStandService.java
@@ -1,7 +1,8 @@
 package application;
 
+import application.utils.BestAlphaCalculator;
 import application.utils.FileUtils;
-import application.utils.MatrixUtils;
+import application.utils.ShellCalculator;
 import org.springframework.boot.CommandLineRunner;
 
 import java.util.List;
@@ -9,11 +10,13 @@ import java.util.List;
 public class MakeItStandService implements CommandLineRunner {
 
     private final FileUtils fileUtils;
-    private final MatrixUtils matrixUtils;
+    private final BestAlphaCalculator bestAlphaCalculator;
+    private final ShellCalculator shellCalculator;
 
-    public MakeItStandService(FileUtils fileUtils, MatrixUtils matrixUtils) {
+    public MakeItStandService(FileUtils fileUtils, BestAlphaCalculator bestAlphaCalculator, ShellCalculator shellCalculator) {
         this.fileUtils = fileUtils;
-        this.matrixUtils = matrixUtils;
+        this.bestAlphaCalculator = bestAlphaCalculator;
+        this.shellCalculator = shellCalculator;
     }
 
     @Override
@@ -21,11 +24,9 @@ public class MakeItStandService implements CommandLineRunner {
         System.out.println("Starting MakeItStand Application");
         String inputName = args[0];
         List<Voxel> voxelsFromXyzFile = fileUtils.createVoxelsFromXyzFile("Inputs/" + inputName + ".xyz");
-        List<Voxel> shell = matrixUtils.calcShellFromVoxels(voxelsFromXyzFile);
+        List<Voxel> shell = shellCalculator.calcShellFromVoxels(voxelsFromXyzFile);
         fileUtils.writeVoxelsToFile(shell, "Outputs/" + inputName + "_shell.xyz");
-        DoubleVoxel balancePoint = matrixUtils.calcBalancePoint(voxelsFromXyzFile);
-        DoubleVoxel initialCenterOfMass = matrixUtils.calcCenterOfMass(voxelsFromXyzFile);
-        List<Voxel> bestAlphaValues = matrixUtils.calcBestValues(balancePoint, initialCenterOfMass, shell, voxelsFromXyzFile);
+        List<Voxel> bestAlphaValues = bestAlphaCalculator.calcBestAlpha(shell, voxelsFromXyzFile);
         fileUtils.writeVoxelsToFile(bestAlphaValues, "Outputs/" + inputName + "_bestAlpha.xyz");
         System.out.println("Finished MakeItStand Successfully");
     }
